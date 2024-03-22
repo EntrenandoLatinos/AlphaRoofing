d86c49ac37765852183ddd9bf90fe2974ed79b74
Libardo Jose Muirillo Tovar <libardomurillotovar@gmail.com>
2024-03-22 08:32:45 -0500

    mail delete from app_core

--- alpha/app_core/urls.py
+++ alpha/app_core/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path('faq/', general.faq, name='faq'),
     path('contact/', general.contact, name='contact'),
     path('privacy/', general.privacy, name='privacy'),
-    path('mail/', general.mail_view, name='mail'),
 ]
